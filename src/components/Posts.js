ca994805d91f846cd5354de58b23966b83224a10
Marcos Couto <marcoscouto1997@gmail.com>
2023-02-11 22:42:33 -0300

    versao com likes e salvar post

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 function Posts() {
 
     const infoPosts = [
@@ -8,7 +10,7 @@ function Posts() {
 
     return (
         <div class="posts">
-            {infoPosts.map(m => 
+            {infoPosts.map(m =>
                 <Post fotoPerfil={m.fotoPerfil} nomePerfil={m.nomePerfil}
                     imagemPost={m.imagemPost} nomeImagemPost={m.nomeImagemPost}
                     curtidaPerfil={m.curtidaPerfil} numCurtidas={m.numCurtidas}
@@ -19,42 +21,81 @@ function Posts() {
 }
 
 function Post(props) {
+
     return (
-            <div data-test="post" class="post">
-                <div class="topo">
-                    <div class="usuario">
+        <div data-test="post" class="post">
+            <div class="topo">
+                <div class="usuario">
                     <img src={props.fotoPerfil} alt={props.nomePerfil} />
-                        {props.nomePerfil}
+                    {props.nomePerfil}
+                </div>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+
+            <div class="conteudo">
+                <img data-test="post-image" src={props.imagemPost} alt={props.nomeImagemPost} />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <CurtirPost name="heart-outline"/>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
+                    <SalvarPost name="bookmark-outline" class="preta"/>
                 </div>
 
-                <div class="conteudo">
-                    <img data-test="post-image" src={props.imagemPost} alt={props.nomeImagemPost} />
-                </div>
-
-                <div class="fundo">
-                    <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
-                            <ion-icon name="chatbubble-outline"></ion-icon>
-                            <ion-icon name="paper-plane-outline"></ion-icon>
-                        </div>
-                        <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
-                        </div>
-                    </div>
-
-                    <div class="curtidas">
-                        <img src={props.imagemPerfilCurtida} alt={props.curtidaPerfil} />
-                        <div data-test="likes-number" class="texto">
-                            Curtido por <strong>{props.curtidaPerfil}</strong> e <strong>outras {props.numCurtidas} pessoas</strong>
-                        </div>
+                <div class="curtidas">
+                    <img src={props.imagemPerfilCurtida} alt={props.curtidaPerfil} />
+                    <div data-test="likes-number" class="texto">
+                        Curtido por <strong>{props.curtidaPerfil}</strong> e <strong data-test="likes-number" >outras {props.numCurtidas} pessoas</strong>
                     </div>
                 </div>
             </div>
+        </div>
+    )
+}
+
+function CurtirPost(props) {
+
+    const [heart, setHeart] = useState("heart-outline")
+    const [cor, setCor] = useState('preta')
+
+    function curtir() {
+        if (heart === "heart-outline") {
+            setHeart("heart")
+            setCor('vermelha')
+        } else {
+            setHeart("heart-outline")
+            setCor('preta')
+        }
+    }
+    return (
+        <ion-icon data-test="like-post" onClick={curtir} class={cor} name={heart}></ion-icon>
+    )
+}
+
+function SalvarPost(props) {
+
+    const [icon, setIcon] = useState("bookmark-outline")
+
+    function salvar() {
+        if (icon === "bookmark-outline") {
+            const novoIcon = "bookmark"
+            setIcon(novoIcon)
+        } else {
+            const novoIcon = "bookmark-outline"
+            setIcon(novoIcon)
+        }
+    }
+
+    return (
+        <div>
+            <ion-icon data-test="save-post" onClick={salvar} name={icon}></ion-icon>
+        </div>
     )
 }
 
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 function Usuario() {
 
     const infoUsuario = { imagem: "assets/img/catanacomics.svg", altImg: "imagem de perfil", nomePerfil: "catanacomics" }
@@ -5,17 +7,33 @@ function Usuario() {
     return (
         <User imagem={infoUsuario.imagem} altImg={infoUsuario.altImg} nomePerfil={infoUsuario.nomePerfil} />
     )
-
 }
 
 function User(props) {
+
+    const [nomePerfil, setNomePerfil] = useState(props.nomePerfil)
+    const [fotoPerfil, setFotoPerfil] = useState(props.imagem)
+
+    function trocarUsuario() {
+        const novoNome = prompt('Digite o nome de usuário');
+        if (novoNome !== '' && novoNome !== undefined && novoNome !== null) {
+            setNomePerfil(novoNome);
+        }
+    }
+
+    function trocarFoto() {
+        const novaFoto = prompt('Digite um novo link');
+        if (novaFoto !== '' && novaFoto !== undefined && novaFoto !== null) {
+            setFotoPerfil(novaFoto)
+        }
+    }
     return (
         <div class="usuario">
-            <img data-test="profile-image" src={props.imagem} alt={props.altImg} />
+            <img onClick={trocarFoto} data-test="profile-image" src={fotoPerfil} alt={props.altImg} />
             <div class="texto">
                 <span>
-                    <strong data-test="name">{props.nomePerfil}</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong data-test="name">{nomePerfil}</strong>
+                    <ion-icon onClick={trocarUsuario} data-test="edit-name" name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
